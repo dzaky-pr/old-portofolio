d9eb834df73063f8e039737b3d56b4ea4da2ad9f
Dzaky Rifai <76737872+dzaky-pr@users.noreply.github.com>
2021-04-09 17:39:47 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -70,3 +70,13 @@ $(document).ready(function(){
         }
     });
 });
+
+//disable cut copy paste
+// $(document).ready(function() {
+// 	$('body').bind('cut copy paste', function (e) {
+// 		e.preventDefault();
+// 	})
+// 	$("body").on("contextmenu", function(e) {
+// 		return false;
+// 	})
+// })
