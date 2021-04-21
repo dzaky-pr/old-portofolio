7e9ae4b59fc00a8274ee978dfdc636608d6ceeba
Dzaky Rifai <76737872+dzaky-pr@users.noreply.github.com>
2021-04-21 09:16:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -72,11 +72,11 @@ $(document).ready(function(){
 });
 
 //disable cut copy paste
-$(document).ready(function() {
-	$('body').bind('cut copy paste', function (e) {
-		e.preventDefault();
-	})
-	$("body").on("contextmenu", function(e) {
-		return false;
-	})
-})
+// $(document).ready(function() {
+// 	$('body').bind('cut copy paste', function (e) {
+// 		e.preventDefault();
+// 	})
+// 	$("body").on("contextmenu", function(e) {
+// 		return false;
+// 	})
+// })
